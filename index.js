983250d55e6dac60f3ff084a09aa7b280d73adcc
seltbr <81486379+seltbr@users.noreply.github.com>
2021-03-27 18:56:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -299,11 +299,11 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["5531983410544@s.whatsapp.net"] // replace this with your number
-			const mod = [ownerNumber,"5531983410544@s.whatsapp.net"]//ubah nomor lo
-			const adminbotnumber = ["5531983410544@s.whatsapp.net"]//ubah nomor lo
-			const frendsowner = ["5531983410544@s.whatsapp.net"]//ubah nomor lo
-			const premium = ["5531983410544@s.whatsapp.net","55983410544@s.whatsapp.net"] 
+			const ownerNumber = ["553183410544@s.whatsapp.net"] // replace this with your number
+			const mod = [ownerNumber,"553183410544@s.whatsapp.net"]//ubah nomor lo
+			const adminbotnumber = ["553183410544@s.whatsapp.net"]//ubah nomor lo
+			const frendsowner = ["553183410544@s.whatsapp.net"]//ubah nomor lo
+			const premium = ["553183410544@s.whatsapp.net","5583410544@s.whatsapp.net"] 
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
